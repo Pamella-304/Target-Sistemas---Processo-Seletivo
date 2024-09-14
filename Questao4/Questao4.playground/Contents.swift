eac26ae2867001ec6106a3a053a96d3a95ee6396
Pamella <pamella.as.304@gmail.com>
2024-09-14 19:35:03 -0300

    quarta questao resolvida

---
+++ Questao4/Questao4.playground/Contents.swift
@@ -0,0 +1,24 @@
+//4) Descubra a lógica e complete o próximo elemento:
+
+//a) 1, 3, 5, 7, ___
+//b) 2, 4, 8, 16, 32, 64, ____
+//c) 0, 1, 4, 9, 16, 25, 36, ____
+//d) 4, 16, 36, 64, ____
+//e) 1, 1, 2, 3, 5, 8, ____
+//f) 2,10, 12, 16, 17, 18, 19, ____
+
+
+
+//RESPOSTA:
+
+//a) -> 1, 3, 5, 7, 9. É uma simples sequência de números ímpares e, por isso, o último elemento é 9.
+
+//b) -> 2, 4, 8, 16, 32, 64, 128. Cada número da sequência corresponde ao dobro do número anterior. Por isso, o último elemento é 128.
+
+//c) -> 0, 1, 4, 9, 16, 25, 36, 49. Os números da sequencia representam os quadrados perfeitos dos números inteiros. Por isso, o último elemento é 49, que é 7 elevado ao quadrado.
+
+//d) -> 4, 16, 36, 64, 100. Essa sequencia corresponde aos quadrados perfeitos dos números pares. Por isso, o ultimo número é 100, que é 10 elevado ao quadrado.
+
+//e) 1, 1, 2, 3, 5, 8, 13. Essa é a sequência de Fibonacci. Por isso, o último elemento é o 13.
+
+//f) 2,10, 12, 16, 17, 18, 19, 20. Aqui, a lógica consiste em alternar entre adicionar 8 e adicionar 1 a números consecutivos.
